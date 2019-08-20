e9d756fd130f85ef0cdbfb729f9f24284d9155b4
mpsingh17 <mr.jassal1722@gmail.com>
2019-08-20 17:43:56 +1200

    submitEvent created in store.

--- calendar_app/src/app/store.js
+++ calendar_app/src/app/store.js
@@ -13,5 +13,12 @@ export const store = {
 				? (dayObj.active = true)
 				: (dayObj.active = false);
 		});
+	},
+	submitEvent(eventDetails) {
+		const activeDay = this.getActiveDay();
+		activeDay.events.push({
+			details: eventDetails,
+			edit: false
+		});
 	}
 };
