85b2319da452b3523f108b4a7ffc59413a3746cc
mpsingh17 <mr.jassal1722@gmail.com>
2019-08-24 21:21:30 +1200

    custom event emit and listen.

--- custom_events/app/main.js
+++ custom_events/app/main.js
@@ -1,10 +1,41 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
-}
+	template: `<input 
+    class="input is-small"
+    type="text"
+    :placeholder="placeholder"
+    v-model="input"
+    @keyup.enter="monitorEnterKey" />`,
+	props: ["placeholder"],
+	data() {
+		return {
+			input: ""
+		};
+	},
+	methods: {
+		monitorEnterKey() {
+			this.$emit("add-note", {
+				note: this.input,
+				timestamp: new Date().toLocaleString()
+			});
+			this.input = "";
+		}
+	}
+};
 
 new Vue({
-  el: '#app',
-  components: {
-    'input-component': inputComponent
-  }
-})
+	el: "#app",
+	data: {
+		notes: [],
+		timestamps: [],
+		placeholder: "Enter a note..."
+	},
+	components: {
+		"input-component": inputComponent
+	},
+	methods: {
+		addNote(event) {
+			this.notes.push(event.note);
+			this.timestamps.push(event.timestamp);
+		}
+	}
+});
