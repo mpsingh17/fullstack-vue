b18f7b3b5be9dfe50937b690ace36bd1d484b2f8
mpsingh17 <mr.jassal1722@gmail.com>
2019-08-25 12:02:42 +1200

    setup and wired store using Vuex.

--- note_taking/app/main.js
+++ note_taking/app/main.js
@@ -1,10 +1,49 @@
+const state = {
+	notes: [],
+	timestamps: []
+};
+
+const mutations = {
+	ADD_NOTE(state, payload) {
+		let newNote = payload;
+		state.notes.push(newNote);
+	},
+	ADD_TIMESTAMP(state, payload) {
+		let newTimeStamp = payload;
+		state.timestamps.push(newTimeStamp);
+	}
+};
+
+const actions = {
+	addNote(context, payload) {
+		context.commit("ADD_NOTE", payload);
+	},
+	addTimestamp(context, payload) {
+		context.commit("ADD_TIMESTAMP", payload);
+	}
+};
+
+const getters = {
+	getNotes: state => state.notes,
+	getTimestamps: state => state.timestamps,
+	getNoteCount: state => state.notes.length
+};
+
+const store = new Vuex.store({
+	state,
+	mutations,
+	actions,
+	getters
+});
+
 const inputComponent = {
-  template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
-}
+	template: `<input placeholder='Enter a note' class="input is-small" type="text" />`
+};
 
 new Vue({
-  el: '#app',
-  components: {
-    'input-component': inputComponent
-  }
-})
+	el: "#app",
+	store,
+	components: {
+		"input-component": inputComponent
+	}
+});
