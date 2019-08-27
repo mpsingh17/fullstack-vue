b6e7311319bcde17144a17c59fda5314727d4ace
mpsingh17 <mr.jassal1722@gmail.com>
2019-08-27 17:25:55 +1200

    set up module for store.

--- shopping_cart/src/app/store/index.js
+++ shopping_cart/src/app/store/index.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import product from "./modules/product/index";
+import cart from "./modules/cart/index";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+	modules: {
+		product,
+		cart
+	}
+});
---
+++ shopping_cart/src/app/store/modules/product/index.js
@@ -0,0 +1,13 @@
+const state = {};
+const mutations = {};
+const actions = {};
+const getters = {};
+
+const productModule = {
+	state,
+	mutations,
+	actions,
+	getters
+};
+
+export default productModule;
