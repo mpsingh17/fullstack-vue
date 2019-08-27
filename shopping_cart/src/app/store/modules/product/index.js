e375bac6ec5a673cbe96184ee283a82302583080
mpsingh17 <mr.jassal1722@gmail.com>
2019-08-27 17:58:47 +1200

    product module is done.

--- shopping_cart/src/app/store/modules/product/index.js
+++ shopping_cart/src/app/store/modules/product/index.js
@@ -1,7 +1,23 @@
-const state = {};
-const mutations = {};
-const actions = {};
-const getters = {};
+import axios from "axios";
+
+const state = {
+	productItems: []
+};
+const mutations = {
+	UPDATE_PRODUCT_ITEMS(state, payload) {
+		state.productItems = payload;
+	}
+};
+const actions = {
+	getProductItems({ commit }) {
+		axios.get("/api/products").then(res => {
+			commit.UPDATE_PRODUCT_ITEMS(res.data);
+		});
+	}
+};
+const getters = {
+	productItems: state => state.productItems
+};
 
 const productModule = {
 	state,
--- shopping_cart/src/main.js
+++ shopping_cart/src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./app/App.vue";
+import store from "./app/store/index";
 
 new Vue({
+	store,
 	render: h => h(App)
 }).$mount("#app");
