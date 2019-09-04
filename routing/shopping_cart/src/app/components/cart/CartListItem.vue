94e558f3cb4addc568801516d0da35a84aae8f5c
mpsingh17 <mr.jassal1722@gmail.com>
2019-09-04 20:51:30 +1200

    displaying cart item images.

--- routing/shopping_cart/src/app/components/cart/CartListItem.vue
+++ routing/shopping_cart/src/app/components/cart/CartListItem.vue
@@ -1,36 +1,33 @@
 <template>
-  <div>
-    <p class="cart-item--title is-inline">{{ cartItem.title }}</p>
-    <div class="is-pulled-right">
-      <i @click="addCartItem(cartItem)"
-        class="fa fa-arrow-circle-up cart-item--modify"></i>
-      <i @click="removeCartItem(cartItem)"
-        class="fa fa-arrow-circle-down cart-item--modify"></i>
+  <div class="box">
+    <div class="cart-item__details">
+      <p class="is-inline">{{ cartItem.title }}</p>
+      <div>
+        <span
+          class="cart-item--price has-text-primary has-text-weight-bold"
+        >{{ cartItem.price }}$ x {{ cartItem.quantity }}</span>
+        <span>
+          <i @click="addCartItem(cartItem)" class="fa fa-arrow-circle-up cart-item__modify"></i>
+          <i @click="removeCartItem(cartItem)" class="fa fa-arrow-circle-down cart-item__modify"></i>
+        </span>
+      </div>
     </div>
-    <div class="cart-item--content">
-      <span class="cart-item--price has-text-primary has-text-weight-bold">
-        ${{ cartItem.price }} each
-      </span>
-      <span class="cart-item--quantity has-text-grey is-pulled-right">
-        Quantity: {{ cartItem.quantity }}
-      </span>
+    <div class="cart-item__image">
+      <img :src="require('../../assets/' + cartItem.image_tag)" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-  name: 'CartListItem',
-  props: ['cartItem'],
+  name: "CartListItem",
+  props: ["cartItem"],
   methods: {
-    ...mapActions([
-      'addCartItem',
-      'removeCartItem'
-    ])
+    ...mapActions(["addCartItem", "removeCartItem"])
   }
-}
+};
 </script>
 
 <style scoped>
