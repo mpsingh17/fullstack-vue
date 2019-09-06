d77850deb5ce26c212e34c9d6e908fa4ba61622d
mpsingh17 <mr.jassal1722@gmail.com>
2019-09-07 11:23:11 +1200

    single product view done.

--- routing/shopping_cart/src/app/components/product/ProductListItem.vue
+++ routing/shopping_cart/src/app/components/product/ProductListItem.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-    <h2 class="has-text-weight-bold">{{ productItem.title }} 
+    <h2 class="has-text-weight-bold">
+      <router-link :to="'/products/' + productItem.id">{{ productItem.title }}</router-link>
+      {{ productItem.title }}
       <span
         @click="addCartItem(productItem)"
-        class="tag is-primary is-pulled-right has-text-white">
-        Add to Cart
-      </span>
+        class="tag is-primary is-pulled-right has-text-white"
+      >Add to Cart</span>
     </h2>
     <p>{{ productItem.description }}</p>
     <span class="has-text-primary has-text-weight-bold">
-      <i class="fa fa-usd"></i> {{ productItem.price }}
+      <i class="fa fa-usd"></i>
+      {{ productItem.price }}
     </span>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-  name: 'ProductListItem',
-  props: ['productItem'],
+  name: "ProductListItem",
+  props: ["productItem"],
   methods: {
-    ...mapActions([
-      'addCartItem'
-    ])
+    ...mapActions(["addCartItem"])
   }
-}
+};
 </script>
 
 <style scoped>
--- routing/shopping_cart/src/app/store/modules/product/index.js
+++ routing/shopping_cart/src/app/store/modules/product/index.js
@@ -1,32 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 const state = {
-  productItems: [] 
-}
+	productItems: []
+};
 
 const mutations = {
-  UPDATE_PRODUCT_ITEMS (state, payload) {
-    state.productItems = payload;
-  }
-}
+	UPDATE_PRODUCT_ITEMS(state, payload) {
+		state.productItems = payload;
+	}
+};
 
 const actions = {
-  getProductItems ({ commit }) {
-    axios.get('/api/products?token=D6W69PRgCoDKgHZGJmRUNA').then((response) => {
-      commit('UPDATE_PRODUCT_ITEMS', response.data)
-    });
-  }
-}
+	getProductItems({ commit }) {
+		axios
+			.get("/api/products?token=D6W69PRgCoDKgHZGJmRUNA")
+			.then(response => {
+				commit("UPDATE_PRODUCT_ITEMS", response.data);
+			});
+	}
+};
 
 const getters = {
-  productItems: state => state.productItems
-}
+	productItems: state => state.productItems,
+	productItemFromId: state => id => {
+		return state.productItems.find(productItem => productItem.id === id);
+	}
+};
 
 const productModule = {
-  state,
-  mutations,
-  actions,
-  getters
-}
+	state,
+	mutations,
+	actions,
+	getters
+};
 
 export default productModule;
